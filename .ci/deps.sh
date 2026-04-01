8513325792c9548f4814274ab02dfc565107d801
Guus Sliepen <guus@tinc-vpn.org>
2026-04-01 22:02:15 +0200

    Fix build issues in CI pipline
    - Don't try to manually compile OpenSSL 3.0 for Fedora and related distros
    - Use meson provided by Debian instead of using pip
    - Make sure to install binfmt-support and pkgconf

--- .ci/deps.sh
+++ .ci/deps.sh
@@ -33,7 +33,7 @@ deps_linux_debian_linux() {
   apt-get update
 
   apt-get install -y \
-    binutils make gcc \
+    binfmt-support binutils gcc make pkgconf \
     zlib1g-dev:"$HOST" \
     libssl-dev:"$HOST" \
     liblzo2-dev:"$HOST" \
@@ -59,7 +59,7 @@ deps_linux_debian() {
 
   apt-get update
   apt-get upgrade -y
-  apt-get install -y git pkgconf sudo texinfo ninja-build
+  apt-get install -y git pkgconf sudo texinfo meson
 
   HOST=${HOST:-}
   if [ "$HOST" = mingw ]; then
@@ -73,16 +73,6 @@ deps_linux_debian() {
   fi
 
   . /etc/os-release
-
-  # Debian Buster ships an old version of meson (0.49).
-  # MinGW cross-compilation requires something newer than 0.55 that ships in Bullseye,
-  # or it fails when looking for dependencies in the OpenSSL wrap.
-  if [ "${ID:-}/${VERSION_CODENAME:-}" = debian/buster ] || [ "$HOST" = mingw ]; then
-    apt-get install -y python3 python3-pip ninja-build
-    pip3 install meson
-  else
-    apt-get install -y meson
-  fi
 }
 
 deps_linux_rhel() {
@@ -100,49 +90,16 @@ deps_linux_rhel() {
 
   yum install -y \
     git binutils make ninja-build pkgconf gcc sudo texinfo-tex systemd perl-IPC-Cmd \
-    lzo-devel zlib-devel lz4-devel ncurses-devel readline-devel libgcrypt-devel systemd-devel "$@"
+    lzo-devel zlib-devel lz4-devel ncurses-devel readline-devel libgcrypt-devel systemd-devel \
+    openssl-devel "$@"
 
   if [ -z "$SKIP_MESON" ]; then
     yum install -y meson
   fi
 
-  if yum info openssl11-devel; then
-    yum install -y openssl11-devel
-  else
-    dnf install -y openssl-devel
-  fi
-
   if yum info miniupnpc-devel; then
     yum install -y miniupnpc-devel
   fi
-}
-
-linux_openssl3() {
-  if [ -n "$SKIP_OPENSSL3" ]; then
-    echo >&2 "skipping openssl3 installation in this job"
-    return
-  fi
-
-  src=/usr/local/src/openssl
-  ssl3=/opt/ssl3
-
-  mkdir -p $src
-
-  git clone --depth 1 --branch openssl-3.0.2 https://github.com/openssl/openssl $src
-  cd $src
-
-  ./Configure --prefix=$ssl3 --openssldir=$ssl3
-  make -j"$(nproc)"
-  make install_sw
-
-  if [ -f /etc/ld.so.conf ]; then
-    echo $ssl3/lib64 >>/etc/ld.so.conf
-    ldconfig -v
-  else
-    ldconfig -v $ssl3/lib64
-  fi
-
-  cd -
 }
 
 deps_linux() {
@@ -157,16 +114,8 @@ deps_linux() {
     deps_linux_debian "$@"
     ;;
 
-  fedora)
+  fedora | centos | almalinux)
     deps_linux_rhel "$@"
-    ;;
-
-  centos | almalinux)
-    deps_linux_rhel "$@"
-
-    if [ "${PLATFORM_ID:-}" != platform:el9 ]; then
-      linux_openssl3
-    fi
     ;;
 
   *) exit 1 ;;
